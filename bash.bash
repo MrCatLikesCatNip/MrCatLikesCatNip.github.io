77cadaa52645432b37d6becbc4d57a9337c1c56a
MrCatLikesCatNip <74006003+MrCatLikesCatNip@users.noreply.github.com>
2024-10-12 20:14:17 -0400

    Delete bash.bash

--- bash.bash
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit of Dynamic Towns game"
-git remote add origin https://github.com/username/dynamic-towns.git
-git push -u origin master
-python -m unittest discover
