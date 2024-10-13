9ae90cd16ec99ad7f3c5233e342d9b60261bad11
MrCatLikesCatNip <74006003+MrCatLikesCatNip@users.noreply.github.com>
2024-10-12 20:12:31 -0400

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -3,3 +3,4 @@ git add .
 git commit -m "Initial commit of Dynamic Towns game"
 git remote add origin https://github.com/username/dynamic-towns.git
 git push -u origin master
+python -m unittest discover
